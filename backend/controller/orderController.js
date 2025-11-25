2c4f848ea1025ad303b82f72dbe2efc9b16ea4a6
Assistantjasmine282 <raijamsine282@gmail.com>
2025-11-25 21:13:15 +0530

    port and payment method change

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -4,7 +4,7 @@ import razorpay from 'razorpay'
  import dotenv from 'dotenv'
  dotenv.config()
  const currency = 'inr'
-const razorpayInstance = new razorpay({
+  const razorpayInstance = new razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET
 })
