34dc81511d4ab6e2860892ebe4b77975b3135311
Assistantjasmine282 <raijamsine282@gmail.com>
2025-11-25 17:21:02 +0530

    Initial commit

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,13 +1,30 @@
-import multer from 'multer'
+// import multer from 'multer'
 
-let storage = multer.diskStorage({
-    destination:(req,file,cb) => {
-        cb(null, Path2D.resolve('./public'))
+// let storage = multer.diskStorage({
+//     destination:(req,file,cb) => {
+//         cb(null, Path2D.resolve('./public'))
+//     },
+//     filename: (req,file,cb)=>{
+//         cb(null, Date.now() + '-' + file.originalname)
+//     }
+// });
+//  let upload = multer({storage})
+
+//  export default upload
+
+
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve("./public"));
     },
-    filename: (req,file,cb)=>{
-        cb(null, Date.now() + '-' + file.originalname)
-    }
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
 });
- let upload = multer({storage})
 
- export default upload
+const upload = multer({ storage });
+
+export default upload;
